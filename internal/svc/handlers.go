68e3868ee377e78f07d2637ebaa6b8e6fd58b7e4
Rafael Mendes <rafael.mendes@unico.io>
2021-03-03 13:59:15 -0300

    refactoring code, removing message.proto, api working as intended

--- internal/svc/handlers.go
+++ internal/svc/handlers.go
@@ -9,19 +9,17 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-// GreeterService struct todos os handlers impostos
-// na definição do nosso serviço gRPC.
+// GreeterService struct
 type GreeterService struct{}
 
-// Greet retorna uma mensagem concatenada com o cumprimento e o nome
-// da pessoa que recebemos na requisição.
+// Greet returns "hello, $name", name received from request
 func (gs GreeterService) Greet(ctx context.Context, req *pb.GreetRequest) (res *pb.GreetResponse, err error) {
-	if req.Msg == nil {
+	if req.Name == "" {
 		err = status.New(codes.InvalidArgument, "Name cannot be empty").Err()
 		return
 	}
 
-	helloMsg := fmt.Sprintf("Hello, %s!", req.Msg.Name)
+	helloMsg := fmt.Sprintf("Hello, %s!", req.Name)
 
 	res = &pb.GreetResponse{
 		Resp: helloMsg,
